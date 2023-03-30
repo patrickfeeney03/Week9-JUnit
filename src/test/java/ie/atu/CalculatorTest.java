b9d24e05c02787a897f038e9a544358867e28a86
Patrick Feeney <g00409592@atu.ie>
2023-03-30 12:03:23 +0100

    Create add test

--- src/test/java/ie/atu/CalculatorTest.java
+++ src/test/java/ie/atu/CalculatorTest.java
@@ -2,9 +2,13 @@ package ie.atu;
 
 import org.junit.jupiter.api.Test;
 
-public class CalculatorTest {
-    @Test
-    void justAnExample() {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+public class CalculatorTest {
+    Calculator calculator = new Calculator();
+    @Test
+    void addTest() {
+        calculator = new Calculator();
+        assertEquals(20, calculator.add(5, 15));
     }
 }
